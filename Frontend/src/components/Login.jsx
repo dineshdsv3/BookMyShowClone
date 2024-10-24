651a06454339905caa131307cd29512e9096c04c
dineshdsv3 <dineshdsv3@gmail.com>
2024-10-24 19:18:01 +0530

    Added registration and login pages in UI

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -2,7 +2,58 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <header className="App-header">
+            <main className="main-area mw-500 text-center px-3">
+                <section>
+                    <h1>Login to BookMyShow</h1>
+                </section>
+                <section>
+                    <Form layout="vertical">
+                        <Form.Item
+                            label="Email"
+                            htmlFor="email"
+                            name="email"
+                            className="d-block"
+                            rules={[{ required: true, message: "Email is Required" }]}
+                        >
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="Enter your Email"
+                            ></Input>
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            htmlFor="password"
+                            name="password"
+                            className="d-block"
+                            rules={[{ required: true, message: "Password is Required" }]}
+                        >
+                            <Input
+                                id="password"
+                                type="password"
+                                placeholder="Enter your Password"
+                            ></Input>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                block
+                                htmlFor="submit"
+                                style={{ fontSize: "1rem", fontWeight: "600" }}
+                            >
+                                Login
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+                <section>
+                    <p>
+                        New User ? <Link to="/register">Register Here</Link>
+                    </p>
+                </section>
+            </main>
+        </header>
     )
 }
 
--- Frontend/src/components/Registration.jsx
+++ Frontend/src/components/Registration.jsx
@@ -2,18 +2,71 @@ import React from 'react'
 
 const Registration = () => {
     return (
-        <div>
-            <header className='App-header'>
-                <main className='main-area mw-500 text-center px-3'>
-                    <section >
-                        <h1>Register to BookMyShow</h1>
-                    </section>
-                    <section>
-                        
-                    </section>
-                </main>
-            </header>
-        </div>
+        <header className="App-header">
+            <main className="main-area mw-500 text-center px-3">
+                <section>
+                    <h1>Register to BookMyShow</h1>
+                </section>
+                <section>
+                    <Form layout="vertical">
+                        <Form.Item
+                            label="Name"
+                            htmlFor="name"
+                            name="name"
+                            className="d-block"
+                            rules={[{ required: true, message: "Name is Required" }]}
+                        >
+                            <Input
+                                id="name"
+                                type="text"
+                                placeholder="Enter your name"
+                            ></Input>
+                        </Form.Item>
+                        <Form.Item
+                            label="Email"
+                            htmlFor="email"
+                            name="email"
+                            className="d-block"
+                            rules={[{ required: true, message: "Email is Required" }]}
+                        >
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="Enter your Email"
+                            ></Input>
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            htmlFor="password"
+                            name="password"
+                            className="d-block"
+                            rules={[{ required: true, message: "Password is Required" }]}
+                        >
+                            <Input
+                                id="password"
+                                type="password"
+                                placeholder="Enter your Password"
+                            ></Input>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                block
+                                htmlFor="submit"
+                                style={{ fontSize: "1rem", fontWeight: "600" }}
+                            >
+                                Register
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+                <section>
+                    <p>
+                        Already a user ? <Link to="/login">Login Now</Link>
+                    </p>
+                </section>
+            </main>
+        </header>
     )
 }
 
